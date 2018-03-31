73a389e0073ec42ba5771d3cc4da521be5f161a4
Florian Weber <florian@wurdackweber.de>
2018-03-31 09:34:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const stores = {};
+
+exports.VuexClass = function(options) {
+    if (typeof options === 'function') {
+        assignStates(options);
+    } else {
+        return (target) => {
+            let store = stores[target.name];
+            
+            if(typeof options.extend !== "undefined") {
+                for (let i = 0; i < Object.keys(options.extend).length; i++) {
+                    const obj = options.extend[Object.keys(options.extend)[i]];
+                    const extendStore = stores[obj.name];
+                    Object.assign(store.state, extendStore.state);
+                    Object.assign(store.getters, extendStore.getters);
+                    Object.assign(store.actions, extendStore.actions);
+                    Object.assign(store.mutations, extendStore.mutations);
+                }
+            }
+            
+            assignStates(target);
+            
+            if (options.moduleName) store['moduleName'] = options.moduleName;
+            
+            if (options.persistent) store['persistent'] = options.persistent;
+            else store['persistent'] = false;
+        };
+    }
+}
+
+exports.Getter = function(target, key, descriptor) {
+    initStore(target);
+    stores[getClassName(target)].getters[key] = target[key];
+}
+
+exports.Mutation = function(target, key, descriptor) {
+    initStore(target);
+    stores[getClassName(target)].mutations[key] = target[key];
+}
+
+exports.Action = function(target, key, descriptor) {
+    initStore(target);
+    stores[getClassName(target)].actions[key] = target[key];
+}
+
+exports.ExportVuexStore = function(target) {
+    return stores[target.name];
+}
+
+function assignStates(Obj) {
+    const target = new Obj();
+    const props = Object.getOwnPropertyNames(target);
+    
+    initStore(target);
+    
+    for(let i = 0; i < Object.keys(props).length; i++) {
+        const prop = props[Object.keys(props)[i]];
+        stores[getClassName(target)].state[prop] = target[prop];
+    }
+}
+
+function initStore(target) {
+    if (typeof stores[getClassName(target)] === 'undefined') {
+        stores[getClassName(target)] = {
+            namespaced: true,
+            state: {},
+            getters: {},
+            actions: {},
+            mutations: {}
+        };
+    }
+}
+
+function getClassName(target) {
+    if (typeof target.constructor.name === 'undefined') {
+        const results = (/function\s([^(]{1,})\(/).exec((target.constructor).toString());
+        return (results && results.length > 1) ? results[1].trim() : '';
+    } else {
+        return target.constructor.name;
+    }
+}
