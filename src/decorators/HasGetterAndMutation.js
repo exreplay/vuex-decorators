96eeda68b14a68373e279d1773e1cd1328006fed
Florian Weber <f.weber@ppm-vi.de>
2019-01-16 09:43:30 +0100

    fixed HasGetterAndMutation decorator where the value, which is getting mutated was not reactive

--- src/decorators/HasGetterAndMutation.js
+++ src/decorators/HasGetterAndMutation.js
@@ -1,9 +1,10 @@
+import Vue from 'vue';
 import { stores, initStore, getClassName } from './utils';
 
 export default function HasGetterAndMutation(target, key, descriptor) {
     initStore(target);
     stores[getClassName(target)].getters[key] = state => state[key];
     stores[getClassName(target)].mutations[key] = (state, val) => {
-        state[key] = val;
+        Vue.set(state, key, val);
     };
 }
