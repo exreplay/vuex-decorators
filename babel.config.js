18b54eb20020251d2e49f25eda1712d52778fdbd
Florian Weber <fl-weber@t-online.de>
2019-12-28 14:40:11 +0100

    refactor: replaced babelrc with babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                modules: "commonjs",
+                targets: {
+                    browsers: "> 0.5%, ie >= 9"
+                }
+            }
+        ]
+    ],
+    plugins: [
+        [ "@babel/plugin-proposal-decorators", { "legacy": true } ],
+        [ "@babel/proposal-class-properties", { "loose" : true } ]
+    ]
+}
