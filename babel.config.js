8f0a91da7763eb13f04f4035dfefb765587b5c5d
Florian Weber <fl-weber@t-online.de>
2019-12-29 09:19:45 +0100

    test: updated babel config with env test

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: 'commonjs',
-        targets: {
-          browsers: '> 0.5%, ie >= 9'
-        }
-      }
-    ]
-  ],
-  plugins: [
-    [ '@babel/plugin-proposal-decorators', { legacy: true } ],
-    [ '@babel/proposal-class-properties', { loose: true } ]
-  ]
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: 'commonjs',
+            targets: {
+              browsers: '> 0.5%, ie >= 9'
+            }
+          }
+        ]
+      ],
+      plugins: [
+        [ '@babel/plugin-proposal-decorators', { legacy: true } ],
+        [ '@babel/proposal-class-properties', { loose: true } ]
+      ]
+    }
+  }
 };
